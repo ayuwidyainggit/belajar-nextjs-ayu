1d23d06d5b449c056f0b9f91b0326b81f8f63284
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-02 15:58:21 +0700

    Tugas 9 Api Routes di Nextjs

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,6 +1,13 @@
 import Layout from "@/layout";
+import { useEffect } from "react";
 
 export default function index() {
+  useEffect(() => {
+    fetch("/api/hello")
+      .then((res) => res.json())
+      .then((res) => console.log("response =>", res))
+      .catch((err) => console.log("err =>", err));
+  }, []);
   return (
     <Layout metaTitle="user" metaDesc="semua informasi seputar user">
       <p>ini user</p>
