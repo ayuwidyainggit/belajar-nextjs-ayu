fb3bdcda31e1c1979c7f18a92cdc698ab4b80236
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-05 22:13:39 +0700

    tugas 11 - getStaticPath

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Content from "@/components/Content";
 import Layout from "@/layout";
+import Link from "next/link";
 
 export default function Notes({ notes }) {
   console.log("notes", notes);
@@ -9,10 +10,11 @@ export default function Notes({ notes }) {
     <>
       <Layout metaTitle="home">
         {notes.data.map((item, index) => (
-          <div style={{ border: "1px solid gray", marginBottom: "2px" }}>
-            <p>{item.title}</p>
-            <p>{item.description}</p>
-          </div>
+          <Link href={`/notes/${item.id}`}>
+            <div style={{ border: "1px solid gray", marginBottom: "2px" }}>
+              <p>{item.title}</p>
+            </div>
+          </Link>
         ))}
       </Layout>
     </>
