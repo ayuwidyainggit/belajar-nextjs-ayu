0cd2c9dc26eacf4c54f35fde9e27133e2521a713
ayuwidyainggit <widyaayu560@gmail.com>
2024-03-05 22:52:06 +0700

    tugas 12 - implementasi Read API untuk crid

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,28 +1,81 @@
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import Content from "@/components/Content";
 import Layout from "@/layout";
-import Link from "next/link";
+import {
+  Flex,
+  Grid,
+  GridItem,
+  Card,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  Heading,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
-export default function Notes({ notes }) {
-  console.log("notes", notes);
+export default function Notes() {
+  const [notes, setNotes] = useState();
+  useEffect(() => {
+    async function fetchingData() {
+      const res = await fetch(
+        "https://paace-f178cafcae7b.nevacloud.io/api/notes"
+      );
+      const listNotes = await res.json();
+      setNotes(listNotes);
+    }
+    fetchingData();
+  }, []);
+
+  console.log("notes nya ", notes);
   return (
     <>
       <Layout metaTitle="home">
-        {notes.data.map((item, index) => (
+        {/* {notes.data.map((item, index) => (
           <Link href={`/notes/${item.id}`}>
             <div style={{ border: "1px solid gray", marginBottom: "2px" }}>
               <p>{item.title}</p>
             </div>
           </Link>
-        ))}
+        ))} */}
+        <Flex>
+          <Grid templateColumns="repeat(3, 1fr)" gap={5}>
+            {notes?.data?.map((item) => (
+              <GridItem>
+                <Card>
+                  <CardHeader>
+                    <Heading>{item.title}</Heading>
+                  </CardHeader>
+                  <CardBody>
+                    <Text>{item.description}</Text>
+                  </CardBody>
+                  <CardFooter
+                    justify="space-between"
+                    flexWrap="wrap"
+                    sx={{
+                      "& > button": {
+                        minW: "136px",
+                      },
+                    }}
+                  >
+                    <Button flex="1" variant="ghost">
+                      Edit
+                    </Button>
+                    <Button flex="1" variant="ghost">
+                      Delete
+                    </Button>
+                  </CardFooter>
+                </Card>
+              </GridItem>
+            ))}
+          </Grid>
+        </Flex>
       </Layout>
     </>
   );
 }
 
-export async function getStaticProps() {
-  const res = await fetch("https://paace-f178cafcae7b.nevacloud.io/api/notes");
-  const notes = await res.json();
-  return { props: { notes }, revalidate: 10 };
-}
+// export async function getStaticProps() {
+//   const res = await fetch("https://paace-f178cafcae7b.nevacloud.io/api/notes");
+//   const notes = await res.json();
+//   return { props: { notes }, revalidate: 10 };
+// }
